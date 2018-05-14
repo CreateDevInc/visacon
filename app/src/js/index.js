0e0f722dc95df039a656b82e41d04fa15ce723b4
Jordin Gardner <gard.jordin@gmail.com>
2018-05-14 14:25:10 -0600

    conditionally include watch file according to env var

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -2,7 +2,17 @@
 // this should only be used to import the other JavaScript
 // files and import the main SCSS file for webpack to bundle it
 
-import './watch';
+if (process.env['CRYOMETRIX_BUILD'] !== 'prod') {
+  console.log('-----------------');
+  console.log('-----------------');
+  console.log('-----------------');
+  console.log('ITS NOT PROD, SO IM IMPORTIGN');
+  console.log('-----------------');
+  console.log('-----------------');
+  console.log('-----------------');
+  require('./watch');
+}
+
 import './product-slider';
 import '../sass/styles.scss';
 import './switch'

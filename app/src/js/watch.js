b6221c5cd661411ede7ede81191c48eb1c860c35
fullnelson14 <bradjnelson14@gmail.com>
2018-06-14 13:49:25 -0600

    removing links/references to product pages

--- app/src/js/watch.js
+++ app/src/js/watch.js
@@ -2,10 +2,9 @@
 // to files that webpack otherwise would not watch
 // (i.e. pug files)
 
-import '../pug/product-t90.pug';
+
 import '../pug/product-t160.pug';
-import '../pug/product-b90.pug';
-import '../pug/product-s90.pug';
+
 import '../pug/templates/product.pug';
 import '../pug/components/footer.pug';
 import '../pug/components/header.pug';
